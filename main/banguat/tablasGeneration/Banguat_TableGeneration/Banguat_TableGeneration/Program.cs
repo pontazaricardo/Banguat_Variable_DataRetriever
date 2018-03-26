90e89114a02fd78e34b68901842c19a2db2a9f8f
pontazaricardo <pontaza.ricardo@gmail.com>
2018-03-26 19:45:44 +0800

    Update

--- main/banguat/tablasGeneration/Banguat_TableGeneration/Banguat_TableGeneration/Program.cs
+++ main/banguat/tablasGeneration/Banguat_TableGeneration/Banguat_TableGeneration/Program.cs
@@ -11,6 +11,7 @@ namespace Banguat_TableGeneration
     class Program
     {
         public static string inputPathFolder = "C:\\Users\\liwuen\\Documents\\GitHub\\Banguat_Variable_DataRetriever\\main\\banguat\\data\\inputs_txt";
+
         public static string outputPathFolder = "C:\\Users\\liwuen\\Documents\\GitHub\\Banguat_Variable_DataRetriever\\main\\banguat\\data\\outputs";
 
         public static List<string> years = new List<string> { "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016" };
