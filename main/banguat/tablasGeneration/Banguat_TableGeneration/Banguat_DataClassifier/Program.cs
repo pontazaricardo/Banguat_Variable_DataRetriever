f4aa32aaf5453f8691e764389b452afc1bdaa215
pontazaricardo <pontaza.ricardo@gmail.com>
2018-03-30 23:27:31 +0800

    Upload

--- main/banguat/tablasGeneration/Banguat_TableGeneration/Banguat_DataClassifier/Program.cs
+++ main/banguat/tablasGeneration/Banguat_TableGeneration/Banguat_DataClassifier/Program.cs
@@ -4,16 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.IO;
+
 namespace Banguat_DataClassifier
 {
     class Program
     {
+        static string inputsFolder = "C:\\Users\\liwuen\\Documents\\GitHub\\Banguat_Variable_DataRetriever\\main\\banguat\\data\\inputs_txt\\";
+
+
+        static List<string> countries = new List<string>() { "belgium", "canada", "costarica", "germany", "italy", "japan", "mexico", "south korea", "taiwan", "usa" };
+        static int initialYear = 2002;
+        static int finalYear = 2017;
+
         static void Main(string[] args)
         {
+            using (var reader = new StreamReader(@""))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    List<string> values = line.Split('\t').ToList();
 
+
+
+                }
+            }
         }
-
-
-
+        
     }
 }
