9964994f5da7b60926f76c52675613cfce2ae682
pontazaricardo <pontaza.ricardo@gmail.com>
2018-04-01 00:46:14 +0800

    Upload

---
+++ main/banguat/tablasGeneration/Banguat_TableGeneration/Banguat_DataClassifier/Country.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banguat_DataClassifier
+{
+    class Country
+    {
+        public string name = "";
+
+        private List<Variable> variables { get; set; }
+
+        public Country(string name)
+        {
+            this.name = name;
+            this.variables = new List<Variable>();
+        }
+        
+    }
+}
